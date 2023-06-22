3f330d7a8270af54f5dc9ccf1983972b9123f895
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-06-22 09:49:59 -0700

    Speed up metro

--- kubernetes/sequencer/scripts/start-metro.sh
+++ kubernetes/sequencer/scripts/start-metro.sh
@@ -2,5 +2,8 @@
 
 set -o errexit -o nounset
 
+sed -i'.bak' 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $home_dir/config/config.toml
+sed -i'.bak' 's/timeout_propose = "10s"/timeout_propose = "1s"/g' $home_dir/config/config.toml
+
 # Start the celestia-app
 exec metro start --home "${home_dir}"
