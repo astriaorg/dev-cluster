6511cc64404177abac9dcafe3360eb91dc76db79
Jesse Snyder <jessetsnyder@gmail.com>
2023-06-29 14:31:01 -0600

    delete config directory before init to ensure clean start

--- kubernetes/celestia-local/scripts/init-celestia-appd.sh
+++ kubernetes/celestia-local/scripts/init-celestia-appd.sh
@@ -2,39 +2,29 @@
 
 set -o errexit -o nounset
 
-# FIXME - why is --overwrite needed now? did celestia-app change how it handles genesis.json creation?
-echo "init"
+rm -rf "$home_dir/config/*"
 celestia-appd init "$chainid" \
   --chain-id "$chainid" \
   --home "$home_dir" \
   --overwrite
 
-echo "keys add"
-
-celestia-appd keys --help
-
 celestia-appd keys add \
   "$validator_key_name" \
   --keyring-backend="$keyring_backend" \
-  --home "$home_dir" \
-  --trace
+  --home "$home_dir"
 
-echo "add-genesis-account"
 validator_key=$(celestia-appd keys show "$validator_key_name" -a --keyring-backend="$keyring_backend" --home "$home_dir")
 celestia-appd add-genesis-account \
   "$validator_key" \
   --home "$home_dir" \
   "$coins"
 
-echo "gentx"
 celestia-appd gentx \
   "$validator_key_name" \
   "$validator_stake" \
   --keyring-backend="$keyring_backend" \
   --chain-id "$chainid" \
   --home "$home_dir" \
-  --orchestrator-address "$validator_key" \
   --evm-address "$evm_address"
 
-echo "collect-gentxs"
 celestia-appd collect-gentxs --home "$home_dir"
