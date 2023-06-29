41c4dd78b3c852722910a4e73fbf884c546b938c
Jesse Snyder <jessetsnyder@gmail.com>
2023-06-28 21:30:10 -0600

    debug echos

--- kubernetes/celestia-local/scripts/init-celestia-appd.sh
+++ kubernetes/celestia-local/scripts/init-celestia-appd.sh
@@ -3,22 +3,30 @@
 set -o errexit -o nounset
 
 # FIXME - why is --overwrite needed now? did celestia-app change how it handles genesis.json creation?
+echo "init"
 celestia-appd init "$chainid" \
   --chain-id "$chainid" \
   --home "$home_dir" \
   --overwrite
 
+echo "keys add"
+
+celestia-appd keys --help
+
 celestia-appd keys add \
   "$validator_key_name" \
   --keyring-backend="$keyring_backend" \
-  --home "$home_dir"
+  --home "$home_dir" \
+  --trace
 
+echo "add-genesis-account"
 validator_key=$(celestia-appd keys show "$validator_key_name" -a --keyring-backend="$keyring_backend" --home "$home_dir")
 celestia-appd add-genesis-account \
   "$validator_key" \
   --home "$home_dir" \
   "$coins"
 
+echo "gentx"
 celestia-appd gentx \
   "$validator_key_name" \
   "$validator_stake" \
@@ -28,4 +36,5 @@ celestia-appd gentx \
   --orchestrator-address "$validator_key" \
   --evm-address "$evm_address"
 
+echo "collect-gentxs"
 celestia-appd collect-gentxs --home "$home_dir"
