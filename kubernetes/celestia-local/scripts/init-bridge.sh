7339b33490d7b45df7204553ebe0187b8d94aa55
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-06-30 17:08:36 -0700

    spacing

--- kubernetes/celestia-local/scripts/init-bridge.sh
+++ kubernetes/celestia-local/scripts/init-bridge.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset
 
 ./celestia bridge init \
   --node.store "$home_dir/bridge" \
-  --core.ip 127.0.0.1\
+  --core.ip 127.0.0.1 \
   --core.rpc.port $celestia_app_host_port \
   --gateway.port $bridge_host_port
 cp -r "$home_dir/keyring-test" "$home_dir/bridge/keys/"
