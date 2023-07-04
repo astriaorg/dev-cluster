14950d9919ce4c2111ca72bc3c51ede9625f0243
Jordan Oroshiba <jordan@astria.org>
2023-07-03 18:34:59 -0700

    Expose WS Port, and cleanup ports across pods (#40)

--- kubernetes/celestia-local/scripts/init-bridge.sh
+++ kubernetes/celestia-local/scripts/init-bridge.sh
@@ -4,9 +4,7 @@ set -o errexit -o nounset
 
 ./celestia bridge init \
   --node.store "$home_dir/bridge" \
-  --core.ip 127.0.0.1
+  --core.ip 127.0.0.1 \
+  --core.rpc.port $celestia_app_host_port \
+  --gateway.port $bridge_host_port
 cp -r "$home_dir/keyring-test" "$home_dir/bridge/keys/"
-
-# must replace the app port used in the bridge config.toml
-sed -i'.bak' "s#Port = \"26657\"#Port = \"$celestia_app_host_port\"#g" $home_dir/bridge/config.toml
-sed -i'.bak' "s#Port = \"26659\"#Port = \"$bridge_host_port\"#g" $home_dir/bridge/config.toml
--- kubernetes/geth/scripts/start-geth.sh
+++ kubernetes/geth/scripts/start-geth.sh
@@ -2,8 +2,8 @@
 
 set -o errexit -o nounset
 
-geth --datadir $home_dir/.astriageth/ --http --http.addr "0.0.0.0" --http.port=$executor_host_http_port \
-  --ws --ws.addr "0.0.0.0" --ws.port=$executor_host_http_port --networkid=912559 --http.corsdomain='*' --ws.origins='*' \
+geth --datadir $home_dir/.astriageth/ --networkid=912559 \
+  --http --http.addr "0.0.0.0" --http.port $executor_host_http_port --http.corsdomain='*' --http.vhosts "*" \
+  --ws --ws.addr "0.0.0.0" --ws.port $executor_host_ws_port --ws.origins='*' \
   --grpc --grpc.addr "0.0.0.0" --grpc.port $executor_host_grpc_port \
-  --http.vhosts "*" \
   --metro.addr "sequencer-service" --metro.port $sequencer_host_grpc_port
