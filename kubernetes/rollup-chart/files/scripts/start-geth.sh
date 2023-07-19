be970354c1164bf480ff82a3e89c9327650cd35a
Jesse Snyder <jessetsnyder@gmail.com>
2023-07-19 14:12:03 -0600

    rename to evmChainId

--- kubernetes/rollup-chart/files/scripts/start-geth.sh
+++ kubernetes/rollup-chart/files/scripts/start-geth.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-geth --datadir $home_dir/.astriageth/ --networkid=$chain_id \
+geth --datadir $home_dir/.astriageth/ --networkid=$evm_chain_id \
   --http --http.addr "0.0.0.0" --http.port $executor_host_http_port --http.corsdomain='*' --http.vhosts "*" \
   --ws --ws.addr "0.0.0.0" --ws.port $executor_host_ws_port --ws.origins='*' \
   --grpc --grpc.addr "0.0.0.0" --grpc.port $executor_host_grpc_port \
