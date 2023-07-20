734ae819cc84a8f52409693be6c3818de009a04b
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-07-20 11:37:10 +0200

    mostly working

--- kubernetes/rollup-chart/files/scripts/start-geth.sh
+++ kubernetes/rollup-chart/files/scripts/start-geth.sh
@@ -2,6 +2,9 @@
 
 set -o errexit -o nounset
 
+export CHAIN_ID=$evm_network_id
+export COMETBFT_RPC_ENDPOINT=$cometbft_rpc_endpoint
+
 geth --datadir $data_dir/ --networkid=$evm_network_id \
   --http --http.addr "0.0.0.0" --http.port $executor_host_http_port --http.corsdomain='*' --http.vhosts "*" --http.api eth,net,web3,debug \
   --ws --ws.addr "0.0.0.0" --ws.port $executor_host_ws_port --ws.origins='*' \
