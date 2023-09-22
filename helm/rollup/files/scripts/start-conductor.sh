aab9cdfa66d30b681aaabadd58132ec00d3423d9
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-09-22 13:03:44 -0700

    Mostly works

--- helm/rollup/files/scripts/start-conductor.sh
+++ helm/rollup/files/scripts/start-conductor.sh
@@ -5,13 +5,15 @@ set -o errexit -o nounset -o pipefail
 # request token from http://celestia-token.astria.localdev.me/ and save to SequencerRelayerConfig.toml
 
 # FIXME - how to use `token-svc` port here instead of hardcoding?
-ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
+BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
 
-if [ -z "$ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN" ]; then
+if [ -z "$BEARER_TOKEN" ]; then
     echo "Failed to fetch the Celestia bearer token."
     exit 1
 fi
 
 echo "Celestia Bearer token fetched successfully."
+
+export ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN="$BEARER_TOKEN"
 
 /usr/local/bin/astria-conductor
--- helm/sequencer/files/scripts/start-relayer.sh
+++ helm/sequencer/files/scripts/start-relayer.sh
@@ -3,15 +3,16 @@
 set -o errexit -o nounset -o pipefail
 
 # FIXME - how to use `token-svc` port here instead of hardcoding?
-ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
+BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
 
-if [ -z "$ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN" ]; then
+if [ -z "$BEARER_TOKEN" ]; then
     echo "Failed to fetch the Celestia bearer token."
     exit 1
 fi
 
 echo "Celestia Bearer token fetched successfully."
 
+export ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN="$BEARER_TOKEN"
 
 /usr/local/bin/astria-sequencer-relayer
 
