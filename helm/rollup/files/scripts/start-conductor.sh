bc4b0f5fdd854fb232905d876230f58cff808d86
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-09-22 10:52:08 -0700

    Update images + configmaps + cometbft startup routine

--- helm/rollup/files/scripts/start-conductor.sh
+++ helm/rollup/files/scripts/start-conductor.sh
@@ -5,21 +5,13 @@ set -o errexit -o nounset -o pipefail
 # request token from http://celestia-token.astria.localdev.me/ and save to SequencerRelayerConfig.toml
 
 # FIXME - how to use `token-svc` port here instead of hardcoding?
-BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
+ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
 
-if [ -z "$BEARER_TOKEN" ]; then
+if [ -z "$ASTRIA_CONDUCTOR_CELESTIA_BEARER_TOKEN" ]; then
     echo "Failed to fetch the Celestia bearer token."
     exit 1
 fi
 
 echo "Celestia Bearer token fetched successfully."
 
-export ASTRIA_celestia_bearer_token="$BEARER_TOKEN"
-
-/usr/local/bin/astria-conductor \
-  --tendermint-url=$cometbft_rpc_endpoint \
-  --celestia-node-url=$celestia_node_url \
-  --chain-id=$evm_chain_id \
-  --libp2p-port=$gossipnet_port \
-  --execution-rpc-url=http://localhost:$executor_host_grpc_port \
-  --bootnodes=/ip4/192.168.65.120/tcp/33900/p2p/12D3KooWJGy9JbZyi4JLF2PsBsuUm8Jn72qrHiQ5it5wygAAvHYb
+/usr/local/bin/astria-conductor
