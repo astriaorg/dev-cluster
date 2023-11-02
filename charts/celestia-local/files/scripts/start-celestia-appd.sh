b3b6cfe865fa1bc36914de300fc304e32fb7d622
Josh Bowen <jskybowen@gmail.com>
2023-11-02 15:31:09 +0100

    first working pass of per rollup bridge node

--- charts/celestia-local/files/scripts/start-celestia-appd.sh
+++ charts/celestia-local/files/scripts/start-celestia-appd.sh
@@ -3,4 +3,4 @@
 set -o errexit -o nounset
 
 # Start the celestia-app
-exec celestia-appd start --home "${home_dir}"
+exec celestia-appd start --home "${home_dir}" --rpc.laddr "tcp://0.0.0.0:26657" 
--- charts/rollup/files/scripts/init-bridge.sh
+++ charts/rollup/files/scripts/init-bridge.sh
@@ -9,7 +9,7 @@ fi
 
 celestia bridge init \
   --node.store "$home_dir/bridge" \
-  --core.ip "celestia-service" \
-  --core.rpc.port $celestia_app_host_port \
+  --core.ip celestia-service \
+  --core.rpc.port $celestia_app_rpc_port \
   --gateway.port $bridge_host_port
 cp -r "$home_dir/keyring-test" "$home_dir/bridge/keys/"
