2c29c9545bd3d15dc2df351f5e269668c44e14dd
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-11-17 11:35:25 +0300

    Enable sequencer relayer to take a passed in token

--- charts/sequencer-relayer/files/scripts/start-relayer.sh
+++ charts/sequencer-relayer/files/scripts/start-relayer.sh
@@ -2,17 +2,19 @@
 
 set -o errexit -o nounset -o pipefail
 
-# FIXME - how to use `token-svc` port here instead of hardcoding?
-BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
+if [ -z "$ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN" ]; then
+    # FIXME - how to use `token-svc` port here instead of hardcoding?
+    BEARER_TOKEN=$(wget -qO- http://celestia-service:5353)
 
-if [ -z "$BEARER_TOKEN" ]; then
-    echo "Failed to fetch the Celestia bearer token."
-    exit 1
+    if [ -z "$BEARER_TOKEN" ]; then
+        echo "Failed to fetch the Celestia bearer token."
+        exit 1
+    fi
+
+    echo "Celestia Bearer token fetched successfully."
+    export ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN="$BEARER_TOKEN"
 fi
 
-echo "Celestia Bearer token fetched successfully."
-
-export ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN="$BEARER_TOKEN"
 
 /usr/local/bin/astria-sequencer-relayer
 
