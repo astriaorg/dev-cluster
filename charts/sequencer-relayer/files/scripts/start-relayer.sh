f3d56577da7321ce3f251a8acaa1ec2c783c9075
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-11-19 20:06:20 +0300

    cleanup logging

--- charts/sequencer-relayer/files/scripts/start-relayer.sh
+++ charts/sequencer-relayer/files/scripts/start-relayer.sh
@@ -3,8 +3,8 @@
 set -o errexit -o nounset -o pipefail
 
 echo "Starting the Astria Sequencer Relayer..."
-echo "ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN: $ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN"
 if [ -z "$ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN" ]; then
+    echo "Fetching the Celestia bearer token..."
     # FIXME - how to use `token-svc` port here instead of hardcoding?
     BEARER_TOKEN=$(wget -qO- $TOKEN_SERVER)
 
@@ -16,8 +16,7 @@ if [ -z "$ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN" ]; then
     echo "Celestia Bearer token fetched successfully."
     export ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN="$BEARER_TOKEN"
 fi
-echo "ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN: $ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN"
-
+echo "ASTRIA_SEQUENCER_RELAYER_CELESTIA_BEARER_TOKEN set"
 
 /usr/local/bin/astria-sequencer-relayer
 
