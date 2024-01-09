7989793370208da049877041547176ab789a2a63
Scott Hurd <WafflesVonMaple@users.noreply.github.com>
2024-01-09 14:15:03 -0800

    Geth: enable prometheus metric on start-geth + service monitor template (#151)
    Co-authored-by: WafflesVonMaple <scott@astria.org>

--- charts/rollup/files/scripts/start-geth.sh
+++ charts/rollup/files/scripts/start-geth.sh
@@ -6,4 +6,6 @@ exec geth --datadir $data_dir/ --networkid=$evm_network_id \
   --http --http.addr "0.0.0.0" --http.port $executor_host_http_port --http.corsdomain='*' --http.vhosts "*" --http.api eth,net,web3,debug,txpool \
   --ws --ws.addr "0.0.0.0" --ws.port $executor_host_ws_port --ws.origins='*' \
   --grpc --grpc.addr "0.0.0.0" --grpc.port $executor_host_grpc_port \
-  {{ if .Values.config.rollup.archiveNode }}--gcmode "archive" --txlookuplimit "0" {{- end }}
+  {{ if .Values.config.rollup.archiveNode }}--gcmode "archive" --txlookuplimit "0" {{- end }} \
+  {{ if .Values.config.rollup.metrics.enabled }} --metrics --metrics.addr "0.0.0.0" --metrics.port {{ .Values.ports.metrics }} {{- end }}
+  
